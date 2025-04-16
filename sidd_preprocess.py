d5538906a267b07ebc59dd91c87e0a2a0a798948
matinsaedi <matinsaedi23@gmail.com>
2025-04-15 21:05:07 -0400

    Update sidd_preprocess.py

--- sidd_preprocess.py
+++ sidd_preprocess.py
@@ -9,8 +9,8 @@ import torchvision.transforms as transforms
 from utils.patcher import save_patches, extract_images_from_lmdb
 
 root_dir = "datasets/SIDD/train"
-train_input_images_folder  = 'a/SIDD/train/input'
-train_target_images_folder = 'a/SIDD/train/target'
+train_input_images_folder  = 'SIDD/train/input'
+train_target_images_folder = 'SIDD/train/target'
 
 # Make sure target folders exist
 os.makedirs(train_input_images_folder, exist_ok=True)
@@ -37,22 +37,22 @@ print("Done moving GT and NOISY files.")
 test_input_lmdb_folder  = 'datasets/SIDD/test/input_crops.lmdb/'
 test_target_lmdb_folder = 'datasets/SIDD/test/gt_crops.lmdb/'
 
-train_input_patches_folder = "a/SIDD/train/input_patches"
+train_input_patches_folder = "SIDD/train/input_patches"
 os.makedirs(train_input_patches_folder, exist_ok=True)
 
-train_target_patches_folder = "a/SIDD/train/target_patches"
+train_target_patches_folder = "SIDD/train/target_patches"
 os.makedirs(train_target_patches_folder, exist_ok=True)
 
-test_input_images_folder = "a/SIDD/test/input"
+test_input_images_folder = "SIDD/test/input"
 os.makedirs(test_input_images_folder, exist_ok=True)
 
-test_target_images_folder = "a/SIDD/test/target"
+test_target_images_folder = "SIDD/test/target"
 os.makedirs(test_target_images_folder, exist_ok=True)
 
-test_input_patches_folder = "a/SIDD/test/input_patches"
+test_input_patches_folder = "SIDD/test/input_patches"
 os.makedirs(test_input_patches_folder, exist_ok=True)
 
-test_target_patches_folder = "a/SIDD/test/target_patches"
+test_target_patches_folder = "SIDD/test/target_patches"
 os.makedirs(test_target_patches_folder, exist_ok=True)
 
 if __name__ == "__main__":
